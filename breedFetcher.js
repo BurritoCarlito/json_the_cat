a2979fd71d158c7de5f18a80976849c486e68a1e
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-04-28 03:19:14 +0000

    refactored breedFetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,22 @@
 const request = require('request');
-const breedName = process.argv[2];
 
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // declaring url from the API given
+const fetchBreedDescription = function(breedName, callback) {
+  
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // declaring url from the API given
 
-request(url, function(error, _response, body) { //request to get content from the internet to my own console
-  if (error) {
-    return console.log('error: ', error);
-  }
-  const data = JSON.parse(body); // converts the JSON string into an acutal object
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log("Breed not found");
-  }
+  request(url, function(error, _response, body) {
+  //request to get content from the internet to my own console
+    if (error) {
+      callback(error, null);
+    } else if (body === '[]') {
+      callback("Unable to find breed, please try again.", null);
+    } else {
+      const data = JSON.parse(body); // converts the JSON string into an acutal object
+      callback(null, data[0].description);
+    }
   //accesses the fist entry in the data array and prints out the description for the user
-});
+  });
+};
 
+module.exports = { fetchBreedDescription };
